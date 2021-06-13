68ca226603732d34056875c3a835f0c7aebbc7ce
C-Lorenzo <59625704+C-Lorenzo-Hub@users.noreply.github.com>
2021-06-13 21:32:08 +0200

    Trees/Rocks tweak + Menu Iteration 1
    Oui

---
+++ U-Metiisto/Assets/Script/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayTheGame()
+    {
+       SceneManager.LoadScene("Test");
+    }
+    public void QuitTheGame()
+    {
+    Debug.Log("QUIT THE GAME");
+    Application.Quit();
+    }
+
+}
