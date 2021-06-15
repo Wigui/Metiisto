cc5f60317849f1b42fa3ca95e7d6e34a25bfcb70
Wigui <gl1999@orange.fr>
2021-06-15 21:41:44 +0200

    Animation Avant
    Il faut rajouter la course dans le code et aussi les animation quand on recul mais c'est good là !

---
+++ U-Metiisto/Assets/Script/AnimationStateController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isrunning = animator.GetBool("isRunning");
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("z");
+        bool runPressed = Input.GetKey("left shift");
+
+        //if player presses up or down
+        if(!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash,true);
+        }
+
+        if(isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash,false);
+        }
+
+        if (!isrunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isrunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+
+    }
+}
